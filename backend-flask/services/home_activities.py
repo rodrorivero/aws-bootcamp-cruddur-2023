d40af1fa496c9527f32b2532901dfbe86f6232df
Rodrigo Rivero <rodrigorivero@outlook.com>
2023-02-27 17:49:00 +0000

    adding honeyvomb to home_acitivies.py

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -8,7 +8,10 @@ class HomeActivities:
   def run():
  
    with tracer.start_as_current_span("home-activities-mock-data"):
+    span = trace.get_current_span()
     now = datetime.now(timezone.utc).astimezone()
+    span.set_attribute("app.now", now.isoformat)
+
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Andrew Brown',
@@ -48,4 +51,5 @@ class HomeActivities:
       'replies': []
     }
     ]
+    span.set_attribute("app.result_length", len(results))
     return results
