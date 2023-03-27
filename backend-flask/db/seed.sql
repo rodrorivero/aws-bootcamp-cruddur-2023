f1af5d575a9777639f2f2a5a7fe443b6f5820b21
Rodrigo Rivero <rodrigorivero@outlook.com>
2023-03-27 00:21:12 +0000

    create db libraries

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -4,10 +4,14 @@ VALUES
   ('Andrew Brown', 'andrewbrown' ,'MOCK'),
   ('Andrew Bayko', 'bayko' ,'MOCK');
 
+SELECT * FROM public.users;
+
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
     (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
-  )
+  );
+
+  SELECT * FROM public.activities;
