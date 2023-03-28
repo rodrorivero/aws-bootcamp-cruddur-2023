8dff256152f3346106a36499bd6430c9b163330e
Rodrigo Rivero <rodrigorivero@outlook.com>
2023-03-28 03:29:33 +0000

    activites coding

--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -49,3 +49,18 @@ class CreateActivity:
         'expires_at': (now + ttl_offset).isoformat()
       }
     return model
+
+  def create_activity(handle, message, expires_at):
+    sql = db.template('activities','create')
+    uuid = db.query_commit(sql,{
+      'handle': handle,
+      'message': message,
+      'expires_at': expires_at
+    })
+    return uuid
+  
+  def query_object_activity(uuid):
+    sql = db.template('activities','object')
+    return db.query_object_json(sql,{
+      'uuid': uuid
+    })
