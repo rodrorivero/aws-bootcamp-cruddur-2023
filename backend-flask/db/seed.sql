7ad9eec775c182795f1c00a57ae5d377f809f10e
Rodrigo Rivero <rodrigorivero@outlook.com>
2023-04-16 02:07:54 +0000

    done dynamodb

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,9 +1,9 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Jeremias Rivero','jere@gmail.com', 'jere_r' ,'c79e4sd8-27a2-48c0-86ad-c4c28dd1a0b4'),
-  ('Abraham Rivero','abraham@outlook.com', 'abraham_r' ,'f3526fb4-a352-4b94-9723-99c61948f908');
-
+  ('Carlos Rivero','rodroriveroe@gmail.com', 'carlos_r' ,'c79e4sd8-27a2-48c0-86ad-c4c28dd1a0b4'),
+  ('Rodrigo Rivero','rodrigorivero@outlook.com', 'rodrigorivero' ,'f3526fb4-a352-4b94-9723-99c61948f908');
+  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 SELECT * FROM public.users;
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
